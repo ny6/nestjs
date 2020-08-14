1561b925455e9b24a853ac6a78b0f998ad107c59
Yeshu Kochher <yeshu@outlook.in>
2020-08-15 03:37:45 +0530

    one where create postgres entity

--- src/modules/coffees/entities/coffee.entity.ts
+++ src/modules/coffees/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
